2ca4db084bb1e8a5c05ce05448858e480e7e9034
Ilya Taziev <tazievii@gmail.com>
2020-06-25 15:33:24 +0500

    mongo db

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/newsanalyzerdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
